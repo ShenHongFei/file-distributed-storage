59ebb42a01566ee192b784f5b88e0db3990ade92
沈鸿飞 <shf19961008@163.com>
2017-07-19 11:09:17 +0800

    Release

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
+## Monitor
+![monitor](doc/monitor.png)
+## 说明书
+![](doc/151.png)
+## echo
+![](doc/152.png)
 
-Netty框架 学习
+![](doc/139.png)
+
+![](doc/138.png)
+
+### 需求
+![大作业.jpg](doc/goal.png)
+
+## Netty架构
+![](doc/netty-architecture.png)
+
+Netty框架 学习参考
 
 https://www.gitbook.com/book/waylau/essential-netty-in-action/details
 
@@ -8,16 +24,3 @@ https://waylau.gitbooks.io/netty-4-user-guide
 https://juejin.im/post/58bbaee6ac502e006b02f607
 
 https://juejin.im/post/58ea47cbda2f60005f070a70
-
-## 演示视频
-[http://v.youku.com/v_show/id_XMjg5MzU3OTczNg==.html?spm=a2h3j.8428770.3416059.1](http://v.youku.com/v_show/id_XMjg5MzU3OTczNg==.html?spm=a2h3j.8428770.3416059.1)
-
-## Monitor
-![monitor](doc/monitor.png)
-
-![说明书](doc/129.png)
-
-### 进度
-![大作业.jpg](doc/大作业.jpg)
-
-
--- 说明书与测试/简单ECHO模型.groovy
+++ 说明书与测试/简单ECHO模型.groovy
@@ -5,39 +5,30 @@ import org.apache.logging.log4j.LogManager
 import spock.lang.Specification
 
 class 简单ECHO模型 extends Specification{
-    
     class EchoServer{
         Server server=new Server(this,6080)
-        
         void echo(ChannelHandlerContext ctx,Map map){
             logger.info("执行echo函数，参数为"+map.toString())
             map.put('result',true)
             ctx.writeAndFlush(map)
         }
     }
-    
     class EchoClient{
         Client client=new Client('127.0.0.1',6080)
-        
         void sendRequest(){
             Map<String,Object> map=new HashMap<>()
             map.put("action","echo")
             map.put("someKey",new Date())
-            
             //发送Map
             client.setRequest(map)
-            
             //阻塞等待并接收Map
             Map<String,Object> response = client.getResponse()
-            
             //输出日志
             logger.info(response)
-            
             //释放连接
             client.channel.close().sync()
         }
     }
-    
     def 'ECHO模型测试'(){
         when:
         new EchoServer()
